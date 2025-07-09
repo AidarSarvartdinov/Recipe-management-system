a3bc442f04bdb954d670016367de90016dc52b68
Aidar <aidarsarvartdinov2004@gmail.com>
2025-07-09 17:59:56 +0300

    modified:   my-app/next.config.ts

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -2,13 +2,13 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack(config) {
-    config.externals = config.externals || [];
-    if (Array.isArray(config.externals)) {
-      config.externals.push('process');
-    }
-    return config;
-  },
+  // webpack(config) {
+  //   config.externals = config.externals || [];
+  //   if (Array.isArray(config.externals)) {
+  //     config.externals.push('process');
+  //   }
+  //   return config;
+  // },
 };
 
 export default nextConfig;
